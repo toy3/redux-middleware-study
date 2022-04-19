1fd3cc3b7a42a39791f721190cfe61a802caefc4
yujin <reverselcf@gmail.com>
2022-04-20 04:05:07 +0900

    proxy 설정

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,11 +2,15 @@ import axios from "axios";
 
 // 두 가지 비동기 함수 (가짜 api)
 export const getPosts = async () => {
-  const response = await axios.get("http://localhost:4000/posts");
+  // const response = await axios.get("http://localhost:4000/posts");
+  // 웹팩 proxy 설정 후
+  const response = await axios.get("/posts");
   return response.data;
 };
 
 export const getPostsById = async (id) => {
-  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  // const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  // 웹팩 proxy 설정 후
+  const response = await axios.get(`/posts/${id}`);
   return response.data;
 };
