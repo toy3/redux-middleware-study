a952c202b30e701917779091cca080dd1ed841ee
yujin <reverselcf@gmail.com>
2022-04-20 04:38:09 +0900

    redux-saga counter component에 적용

--- src/index.js
+++ src/index.js
@@ -5,25 +5,31 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 // import myLogger from "./middlewares/myLogger"; // 직접 만든 로거
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import createSagaMiddleware from "redux-saga";
 
 const customHistory = createBrowserHistory();
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   composeWithDevTools(
     applyMiddleware(
+      sagaMiddleware,
       ReduxThunk.withExtraArgument({ history: customHistory }),
       logger
     )
   ) // 미들웨어 적용(myLogger, logger). 다른 미들웨어랑 logger를 함께 사용할 경우 logger가 맨 끝에 가도록 해줘야 함.
 );
+
+// store를 먼저 만든 후에 작업
+sagaMiddleware.run(rootSaga);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // 프로젝트에 리덕스 적용해주기 === Provider, store 적용해주기
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,17 +1,41 @@
-// ducks 패턴 모듈
+import { delay, put, takeEvery, takeLatest } from "redux-saga/effects";
 
-// 액션 타입
+////// ducks 패턴 모듈
+
+////// [ 액션 타입 ]
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
+const INCREASE_ASYNC = "INCREASE_ASYNC";
+const DECREASE_ASYNC = "DECREASE_ASYNC";
 
-// 액션 생성함수
+////// [ 액션 생성함수 ]
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
 
-// 초기상태
+////// [ saga 작성 ] generator 함수를 사용해서 선언한다.
+// effects는 yield를 해주어야 한다.
+function* increaseSaga() {
+  yield delay(1000); // effects를 yield 한다.
+  yield put(increase()); // put은 dispatch 와 비슷한 것.
+}
+
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+// 내보내줘야 한다. rootSaga를 만들기 위해.
+export function* counterSaga() {
+  yield takeEvery(INCREASE_ASYNC, increaseSaga); // 추후 INCREASE_ASYNC 액션이 dispatch 될 때마다 increaseSaga 함수를 실행해준다.
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga); // 추후 DECREASE_ASYNC 액션이 dispatch 될 때 가장 마지막으로 들어오면 액션에 대해서만 decreaseSaga 함수를 실행해준다. 빨리 여러번 클릭하면 딱 1만 마이너스됨.
+}
+
+////// [ 초기상태 ]
 const initialState = 0;
 
-// 리듀서
+////// [ 리듀서 ]
 export default function counter(state = initialState, action) {
   switch (action.type) {
     case INCREASE:
@@ -22,18 +46,3 @@ export default function counter(state = initialState, action) {
       return state;
   }
 }
-
-// 특정 thunk 함수가 dispatch 되면 1초 후에 increase 또는 decrease 를 dispatch 하기
-// 두번째 파라미터 (dispatch, getState) 부터 끝까지가 thunk 함수이고,
-// 첫번째 파라미터부터 끝까지는 thunk 함수를 만들어주는 함수(thunk creator) 이다.
-export const increaseAsync = () => (dispatch, getState) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
-
-export const decreaseAsync = () => (dispatch, getState) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
