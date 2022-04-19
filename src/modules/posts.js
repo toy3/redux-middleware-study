fb642559ca39384dd27e2700ecea9ff41c0dc86c
yujin <reverselcf@gmail.com>
2022-04-20 05:18:35 +0900

    redux-saga에 라우터 연동

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -6,7 +6,7 @@ import {
   handleAsyncActionsById,
   reducerUtils,
 } from "../lib/asyncUtils";
-import { call, put, takeEvery } from "redux-saga/effects";
+import { call, put, takeEvery, getContext } from "redux-saga/effects";
 
 //getPosts 에 대한 ACTION
 const GET_POSTS = "GET_POSTS"; // 특정 요청이 시작됐다를 알리는 ACTION
@@ -21,6 +21,9 @@ const GET_POST_ERROR = "GET_POST_ERROR";
 // 초기화 ACTION
 const CLEAR_POST = "CLEAR_POST";
 
+// HOME으로 가기 ACTION
+const GO_TO_HOME = "GO_TO_HOME";
+
 ////// [ thunk 생성함수 ]
 // saga 액션 객체
 export const getPosts = () => ({ type: GET_POSTS });
@@ -33,17 +36,26 @@ export const getPost = (id) => ({
 // saga 함수들
 const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
 const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostsById);
+function* goToHomeSaga() {
+  const history = yield getContext("history");
+  history.push("/");
+}
 
 // 위 리덕스 모듈을 위한 saga를 모니터링 해주는 함수. rootSaga에 등록해준다.
 export function* postsSaga() {
   yield takeEvery(GET_POSTS, getPostsSaga);
   yield takeEvery(GET_POST, getPostSaga);
+  yield takeEvery(GO_TO_HOME, goToHomeSaga);
 }
 
-export const goToHome = () => (dispatch, getState, extra) => {
-  const { history } = extra;
-  history.push("/");
-};
+// custom history 사용하는 thunk 함수
+// export const goToHome = () => (dispatch, getState, extra) => {
+//   const { history } = extra;
+//   history.push("/");
+// };
+
+// saga의 history 사용하는 함수
+export const goToHome = () => ({ type: GO_TO_HOME });
 
 // 초기화
 export const clearPost = () => ({ type: CLEAR_POST });
